1e968b2411a2d1d789854d63ebb905d72ff2a597
Kacper Kalinowski <kacper.kalinowski@intive.com>
2022-11-22 13:40:07 +0100

    Migrate from springfox to springdoc and add actuator

--- server-side/src/main/java/server/config/SwaggerConfiguration.java
+++ server-side/src/main/java/server/config/SwaggerConfiguration.java
@@ -2,10 +2,6 @@ package server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Responsible for setting up swagger.
@@ -13,14 +9,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @author kacper.kalinowski
  */
 @Configuration
-@EnableSwagger2
 public class SwaggerConfiguration {
 
-  @Bean
-  public Docket swaggerApi() {
-    return new Docket(DocumentationType.SWAGGER_2)
-        .select()
-        .paths(PathSelectors.regex("^(?!/(error).*$).*$"))
-        .build();
-  }
+//  @Bean
+//  public Docket swaggerApi() {
+//    return new Docket(DocumentationType.SWAGGER_2)
+//        .select()
+//        .paths(PathSelectors.regex("^(?!/(error).*$).*$"))
+//        .build();
+//  }
 }
